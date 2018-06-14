612c24bfae834cb34acb993911cfb0184d711d27
Martin Hofherr <martin.hofherr-ext@eos.info>
2018-06-14 15:01:13 +0200

    Added bcd_converter_model

---
+++ bin2bcd/test/bcd_converter_model.py
@@ -0,0 +1,15 @@
+# This is the model that provides a conversion of integer numbers to
+# binary BCD representation.
+
+import math as m
+
+def int2digits(num):
+    ndigs = m.floor(m.log10(num))
+    bcd = 0                        
+    for i in range(ndigs, -1, -1):
+        dig = num // pow(10,i)
+        num = num - pow(10, i) * dig
+        
+        bcd = bcd + dig * pow(16, i)
+
+    return bcd
